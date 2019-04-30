fcc592b24aecd9efd41e79e7bdbb54b03bcde5be
lucas.portet <lucasportet@gmail.com>
2019-04-30 21:35:12 +0200

    Vuex folder name changed to 'Store'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-import store from './vuex';
+import store from './store';
 import api from './api';
 
 Vue.config.productionTip = false;
